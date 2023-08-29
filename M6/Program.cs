087328d5d92567315eb7e45e5be2d868375a12f8
santiarroyave <santi98cv@hotmail.com>
2023-08-29 03:31:36 +0200

    Milestone 1: Fase 1

---
+++ M6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace M6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Volem fer un software per un taller de vehicles, que en aquest moment té motos i cotxes. Els cotxes sempre tindran quatre rodes i les motos dues.
+
+            //MILESTONE 1
+            //FASE 1:
+            //S’ha de crear un projecte, C# de consola, amb les funcionalitats demanades per tal que el software funcione correctament.
+
+            //Ens demanen crear un metode Main que realitzi els següents passos:
+            //  1) Demanar a l’usuari la matrícula del cotxe, la marca i el seu color.
+            //  2) Crear el cotxe amb les dades anteriors.
+            //  3) Afegir-li dues rodes traseres demanant a l’usuari la marca i el diametre.
+            //  4) Afegir-li dues rodes davanteres demanant a l’usuari la marca i el diametre.
+
+            var (matricula, marca, color) = DemanarDadesCotxe();
+            Cotxe cotxe1 = new Cotxe(matricula, marca, color);
+            cotxe1.AfegirRodesDavanteres();
+            cotxe1.AfegirRodesTraseres();
+
+            cotxe1.ToString();
+        }
+
+        public static (string Matricula, string Marca, string Color) DemanarDadesCotxe()
+        {
+            Console.WriteLine("Ingressa la matrícula del cotxe:");
+            string matricula = Console.ReadLine();
+
+            Console.WriteLine("Ingressa la marca:");
+            string marca = Console.ReadLine();
+
+            Console.WriteLine("Ingressa el color:");
+            string color = Console.ReadLine();
+
+            return (matricula, marca, color);
+        }
+    }
+}
---
+++ M6/Vehicles.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M6
+{
+    internal class Vehicles
+    {
+        int quntitatRodes;
+    }
+
+    internal class Moto : Vehicles
+    {
+        const int DEF_QUANT_RODES = 2;
+        //public Motos()
+        //{
+
+        //}
+    }
+
+    internal class Cotxe : Vehicles
+    {
+        const int DEF_QUANT_RODES = 4;
+
+        string matricula;
+        string marca;
+        string color;
+        Rodes rodesDavanteres;
+        Rodes rodesTraseres;
+
+        public Cotxe(string matricula, string marca, string color)
+        {
+            this.matricula = matricula;
+            this.marca = marca;
+            this.color = color;
+        }
+
+        public void AfegirRodesDavanteres()
+        {
+            Console.WriteLine("\nEstàs seleccionant les rodes davanteres");
+            Console.WriteLine("Indica la marca:");
+            string marca = Console.ReadLine();
+            Console.WriteLine("Indica el diàmetre:");
+            int diametre = int.Parse(Console.ReadLine());
+
+            rodesDavanteres = new Rodes(marca, diametre);
+        }
+
+        public void AfegirRodesTraseres()
+        {
+            Console.WriteLine("\nEstàs seleccionant les rodes traseres");
+            Console.WriteLine("Indica la marca:");
+            string marca = Console.ReadLine();
+            Console.WriteLine("Indica el diàmetre:");
+            int diametre = int.Parse(Console.ReadLine());
+
+            rodesTraseres = new Rodes(marca, diametre);
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine("\nInformació del cotxe:");
+            Console.WriteLine("- Matricula: {0}", matricula);
+            Console.WriteLine("- Marca: {0}", marca);
+            Console.WriteLine("- Color: {0}", color);
+            Console.WriteLine("- Rodes davanteres: {0}", rodesDavanteres.ToString());
+            Console.WriteLine("- Rodes traseres: {0}", rodesTraseres.ToString());
+        }
+
+    }
+
+    internal class Rodes
+    {
+        string marca;
+        int diametre;
+
+        public Rodes(string marca, int diametre)
+        {
+            this.marca = marca;
+            this.diametre = diametre;
+        }
+
+        public string ToString()
+        {
+            return string.Format("Rodes {0} de diàmetre {1} cm de diàmetre.", marca, diametre);
+        }
+    }
+}
